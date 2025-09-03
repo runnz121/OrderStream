3b067be0f49cd57668e7e132c72c9ea9e007ec05
cwkim <heatarin@gmail.com>
2025-09-03 23:59:48 +0900

    [feat] 공통 slack util 추가 (토큰 환경변수 주입 필요)

--- stock/src/main/kotlin/io/readingrecord/stock/StockApplication.kt
+++ stock/src/main/kotlin/io/readingrecord/stock/StockApplication.kt
@@ -1,8 +1,11 @@
 package io.readingrecord.stock
 
+import io.readingrecord.common.component.SlackComponent
+import org.springframework.boot.ApplicationRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.ApplicationContext
+import org.springframework.context.annotation.Bean
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -14,6 +17,16 @@ class StockApplication {
 
         @GetMapping
         fun index() = "%s is running!".format(applicationContext.id)
+    }
+
+    @Bean
+    fun applicationStartup(slackComponent: SlackComponent) = ApplicationRunner {
+        try {
+            slackComponent.sendMessage("🚀 Stock Service가 시작되었습니다!", "C09D8SDSLE9")
+            println("Slack 테스트 메시지 전송 완료")
+        } catch (error: Exception) {
+            println("Slack 테스트 메시지 전송 실패: ${error.message}")
+        }
     }
 }
 
