01683ba4ad71023430b969de1debf21e8fb774f5
runnz121 <runnz121@gmail.com>
2025-09-03 22:24:51 +0900

    [feat] gradle 설정 추가, r2dbc config 추가 - r2dbc, webflux 의존성 추가 - r2dbc 환경설정 파일 생성

--- return/build.gradle.kts
+++ return/build.gradle.kts
@@ -1,8 +1,23 @@
 dependencies {
     implementation(project(":common"))
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+    implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
+
+    implementation("io.asyncer:r2dbc-mysql:1.3.2")
+
+    implementation("io.r2dbc:r2dbc-pool")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("io.projectreactor:reactor-test")
+}
+
+configurations {
+    implementation {
+        exclude(group = "org.springframework.boot", module = "spring-boot-starter-web")
+    }
 }
 
 tasks.withType<org.springframework.boot.gradle.tasks.bundling.BootJar> {
     mainClass.set("io.readingrecord.order.ReturnApplicationKt")
 }
-
